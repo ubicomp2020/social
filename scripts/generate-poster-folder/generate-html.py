b5eebd0d8da5d2b16ed8d47cc2e21b8d24a4ebbb
Danilo Gasques <danilod100@gmail.com>
2020-09-11 20:01:29 -0700

    generated html files

--- scripts/generate-poster-folder/generate-html.py
+++ scripts/generate-poster-folder/generate-html.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import io
 import csv2map
 
 contentNotFoundLink = "index.html"
@@ -10,24 +11,24 @@ if len(sys.argv) < 4:
     sys.exit(1)
 
 # poster_default_glow.save("default-poster-glow.png")
-with open(argv[1]) as f:
-    templateHTML = f.readlines()
+with io.open(sys.argv[1],'r',encoding='utf8') as f:
+    templateHTML = "".join(f.readlines())
 posterMap = csv2map.transform(sys.argv[2], 'ID', 'ID in PCS')
 outpath = sys.argv[3]
 
 for folder in posterMap:
+    # cleans title from any weird html tag / character
+    title = posterMap[folder]["Title(49)"].replace('&', '&amp;').replace('<', '&lt;').replace('>', '&gt;').replace('"', '&quot;').replace("'", '&#39;')
+    
+    # remove trailing spaces
+    link = posterMap[folder]["main content"].strip()
+    if len(link) == 0:
+        link = contentNotFoundLink
+    
+    outputName = posterMap[folder][r"Whova/Gather"].replace(" ", "").lower()    
+    with io.open(os.path.join(outpath, "%s.html" % outputName),'w',encoding='utf8') as f:
+        f.write(templateHTML % (link, title, link))
 
-
-                
-    outputName = posterMap[folder][r"Whova/Gather"].replace(" ", "").lower()
-    if candidateThumbnail is not None:
-        candidateThumbnail.save(os.path.join(outpath, "%s.png" % outputName))
-        candidateThumbnail.paste(glow, (0,0), glow)
-        candidateThumbnail.save(os.path.join(outpath, "%sa.png" % outputName))
-    else:
-        poster_default.save(os.path.join(outpath, "%s.png" % outputName))
-        poster_default_glow.save(os.path.join(outpath, "%sa.png" % outputName))
-        
-    print("--> saved to %s and %s to %s " % ("%s.png" % outputName, "%sa.png" % outputName, outpath))
+    print("--> saved to %s to %s " % ("%s.html" % outputName, outpath))
 
 
